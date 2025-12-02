951d50ea9a1db8199813ba7fc15ab20b0cc9a176
ipatel-garc <ipatelgarcia@gmail.com>
2025-12-02 19:04:48 +0000

    Nested Lists

--- week 13  csp/4 — Nested Lists and List Comprehensions.py
+++ week 13  csp/4 — Nested Lists and List Comprehensions.py
@@ -1,7 +1,56 @@
 # Objective:
 # Students will manipulate nested lists and understand basic list comprehensions.
 
-# Key Notes:
+
+list1 = [1,2,3]
+print(list1[-1])
+list2= [4,5,6]
+print(list2[0])
+nested_list= [list1,list2]
+print(nested_list[0]) # Output 1,2,3
+print(nested_list[1]) #Output 4,5,6
+print(nested_list[0][1]) # Output 2
+print(nested_list[1][1]) # Output 5
+print(nested_list[1][0]) # Output 4
+
+
+fruits= ["apple", "orange", "banana", "coconut"]
+vegetables= ["celery", "carrots", "potatos"]
+meats = ["chicken", "fish", "turkey"]
+
+groceries= [fruits,vegetables,meats]# Key Notes:
+print(groceries[2][-1]) # Output turkey
+print(groceries[1][1]) # Output carrots
+print(groceries[0][2]) # Output banana
+
+for collection in groceries:
+    for food in collection:
+        print(food, end= " ")
+    print()
+
+num_pad = ((1,2,3),
+           (4,5,6),
+           (7,8,9),
+           ("*", 0, "#"))
+
+for row in num_pad:
+    for num in row:
+        print(num, end= " ")
+    print()
+
+
+
+#nested loop
+
+# for i in range (1,1001):     #outer loop
+#     for j in range (1,1001):
+#         if i>0 and j>0:
+#             for k in range (1,1001):
+#                 print("the number is", i,j,k) # inner loop
+
+
+
+
 
 # A list can contain other lists.
 
@@ -18,28 +67,36 @@
 
 # Examples:
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
 
-print(matrix[1][2])    # 6
+# print(matrix[1][2])    # 6
 
-# List comprehension
-first_col = [row[0] for row in matrix]
-print(first_col)       # [1, 4, 7]
+# # List comprehension
+# first_col = [row[0] for row in matrix]
+# print(first_col)       # [1, 4, 7]
 
 
 
 # Practice Problems:
 
 # Build a matrix variable containing 3 lists of 3 numbers each.
-
+matrix = [
+    [10,20,30],
+    [40,50,60],
+    [70,80,90]
+]
 # Print the first list.
-
+print(matrix[0])
 # Print the second item from the third list.
+print(matrix[2][1])
 
 # Use a list comprehension to extract the last item from each sub-list.
-
+sum_list= [row[-1] for row in matrix]
+print(sum_list)
 # Challenge: Create a new list containing squares of numbers from 1–10 using a comprehension.
+squares = [x**2 for x in range(1,11)]
+print(squares)
